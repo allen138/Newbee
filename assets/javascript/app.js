5e6c54b0f7caa136b133f8eb0d203259677cc8d4
Allen M <tmracing008@yahoo.com>
2018-10-30 18:11:43 -0700

    add script links

---
+++ assets/javascript/app.js
@@ -0,0 +1,10 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyAaBxgXJBVE_9C8qzRu_ebV1sscAVqiers",
+    authDomain: "group-project-4da86.firebaseapp.com",
+    databaseURL: "https://group-project-4da86.firebaseio.com",
+    projectId: "group-project-4da86",
+    storageBucket: "group-project-4da86.appspot.com",
+    messagingSenderId: "847074672907"
+  };
+  firebase.initializeApp(config);
