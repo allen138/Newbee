0b9b4ec6ca9fdfe3565b4196600fe5307fdf1da4
Allen M <tmracing008@yahoo.com>
2018-11-03 15:05:23 -0700

    style changes

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -10,22 +10,35 @@ var config = {
 firebase.initializeApp(config);
 
 var database = firebase.database();
+var ip = "";
+getIP();
+function getIP() {
+    $.getJSON('http://ipinfo.io', function (data) {
+        ip = data.ip;
+        getEvents();
+        return ip;
+    });
+}
+function getEvents() {
+    var client_id = "MTM3Njk0OTN8MTU0MTEyNjYzMS42OQ";
+    var geoAPI = "geoip=" + ip;
+    var range = "range=15mi";
+    var url = "https://api.seatgeek.com/2/events?client_id=" + client_id + "&" + geoAPI+"&"+range;
+    $.ajax({
+        url: url,
+        method: "GET"
+    }).then(function (response) {
+        console.log(response);
+    });
+}
 
 // click function rendering search input. 
 $("#search").on("click", function (event) {
   event.preventDefault();
-  console.log("clicked");
-  var search = $("input:checked").val();
-  console.log(search);
-  // var queryURL = "https://api.seatgeek.com/2/events?client_id=MTM3MzYxMTF8MTU0MDg2OTY4OS40NQ";
-
-  // $.ajax({
-  //   url: queryURL,
-  //   method: "GET",
-  // }).then(function (response) {
-  //   console.log(response);
-    
-  // });
+  $.each($("input:checked"), function() {
+    var search = $(this).val();  
+    console.log(search);
+  });
 });
 // function to render api results to ui
 function renderResults(results) {
