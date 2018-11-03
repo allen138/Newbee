3e1472b9e2529de1fa895fe08d3074c337f886f1
Allen M <tmracing008@yahoo.com>
2018-11-03 10:23:18 -0700

    create basic ui

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -10,17 +10,37 @@ var config = {
 firebase.initializeApp(config);
 
 var database = firebase.database();
-$(".btn").on("click", function (event) {
+
+// click function rendering search input. 
+$("#search").on("click", function (event) {
   event.preventDefault();
   console.log("clicked");
-  var search = $("#search").val().trim();
+  var search = $("input:checked").val();
   console.log(search);
-  var queryURL = "https://api.seatgeek.com/2/events?client_id=MTM3MzYxMTF8MTU0MDg2OTY4OS40NQ";
+  // var queryURL = "https://api.seatgeek.com/2/events?client_id=MTM3MzYxMTF8MTU0MDg2OTY4OS40NQ";
 
-  $.ajax({
-    url: queryURL,
-    method: "GET",
-  }).then(function (response) {
-    console.log(response);
-  });
+  // $.ajax({
+  //   url: queryURL,
+  //   method: "GET",
+  // }).then(function (response) {
+  //   console.log(response);
+    
+  // });
 });
+// function to render api results to ui
+function renderResults(results) {
+
+  var events = $("<ul>");
+  events.addClass("list-group");
+  $(".events").append(events);
+
+  var title = results.events.title;
+  var eventsList = $("<li class='event-list-title'");
+  eventsList.append("<span class='label label-primary'>" + title + "</span>");
+
+  var eventDate = results.events.datetime_local;
+  eventsList.append("<h5>" + eventDate + "</h5>");
+
+  events.append(eventsList);
+}
+
